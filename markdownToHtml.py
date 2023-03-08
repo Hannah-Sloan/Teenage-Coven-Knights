22986c0c480a2840622da0a077ca76c833a48490
Hannah Sloan <hannah.ava.sloan@gmail.com>
2023-03-08 15:07:43 -0700

    Attempting to favicon chrome

--- markdownToHtml.py
+++ markdownToHtml.py
@@ -64,8 +64,8 @@ indent_depth += "\t"
 html += indent_depth + "<head>\n"
 indent_depth += "\t"
 html += indent_depth + "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />\n" #Adding Stylesheet
-html += indent_depth + "<link rel=\"shortcut icon\" href=\"img/moon.ico\"/>\n" #Adding Favicon
-html += indent_depth + "<link rel=\"icon\" type=\"image/x-icon\" href=\"https://hannah-sloan.github.io/Teenage-Coven-Knights/img/moon.ico\">\n"
+html += indent_depth + "<link rel=\"shortcut icon\" href=\"img/moon.ico\" type=\"image/x-icon\"/>\n" #Adding Favicon
+html += indent_depth + "<link rel=\"icon\" href=\"https://hannah-sloan.github.io/Teenage-Coven-Knights/img/moon.ico\" type=\"image/x-icon\"/>\n"
 html += indent_depth + "<title> "
 if((input_file[:len(input_file)-3]) == "index"): #Adding Title
     html += "Home - Teenage Coven Knights" 
